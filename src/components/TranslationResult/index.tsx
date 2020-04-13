2283c8582e544618c9f924bef9330b2974f1dc13
Julian Flynn <julian.flynn@ezcater.com>
2020-04-13 03:04:07 -0400

    Working basic version of codepoint to unicode while typing

--- src/components/TranslationResult/index.tsx
+++ src/components/TranslationResult/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import styled from '@emotion/styled/macro'
 import { ITranslationState } from '../../actions';
 
@@ -14,10 +14,29 @@ const StyledColumn = styled.div`
   min-height: 160px;
 `;
 
+function codePointToUnicode(input: string | undefined): string | null {
+  if (input) {
+    const sanitized = parseInt(input, 16);
+
+    return String.fromCodePoint(sanitized);
+  }
+
+  return null;
+}
+
 function TranslationResult({ contentParts = [], codePointsDictionary = {} }: ITranslationState) {
+  if (!contentParts.length) {
+    return <StyledColumn>Translation</StyledColumn>;
+  }
+
   return (
     <StyledColumn>
-      {contentParts.join(' ') || 'Translation'}
+      {contentParts.map((part, index) => {
+        const unicode = codePointToUnicode(codePointsDictionary[part]);
+        const text = unicode || part;
+
+        return <Fragment key={text + index}>{' '}{text}</Fragment>;
+      })}
     </StyledColumn>
   );
 };
